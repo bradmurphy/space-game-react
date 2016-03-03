5c34e389f863634f61cce6187000d0f854cc1ca9
Brad Murphy <brad.murphy@byhook.com>
2016-03-03 15:54:07 -0500

    Bringing in hook scaffold and then implementing React.

--- app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-/**
- * Require and instantiate the express server
- */
-const server = require('./server')();
--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,14 @@
 'use strict';
 
-const gulp = require('gulp');
-const dns = require('dns');
-const os = require('os');
-const open = require('open');
-const refresh = require('gulp-livereload');
-const livereload = require('connect-livereload');
-const server = require('./server')(livereload);
+/**
+ * gulpfile.js
+ * ===========
+ * Rather than manage one giant configuration file responsible
+ * for creating multiple tasks, each task has been broken out into
+ * its own file in gulp/tasks. Any file in that folder gets automatically
+ * required by the loop in ./gulp/index.js (required below).
 
-gulp.task('serve', () => {
+ * To add a new task, simply add a new task file to gulp/tasks.
+ */
 
-  // lookup the local ip and open the browser with it (instead of localhost).
-  // This is so the 2nd screen url works on local networks
-
-  dns.lookup(os.hostname(), function (err, address, family) {
-
-    let url = 'http://' + address + ':' + server.port;
-
-    console.log('http server started @', url);
-    open(url);
-
-  });
-
-});
-
-gulp.task('livereload', () => {
-
-  // livereload should start listening on the server livereload port (35729)
-
-  refresh.listen(server.lrport);
-
-});
-
-gulp.task('watch', () => {
-
-  // watch the public and bower_components directories for changes
-
-  gulp.watch('public/**').on('change', refresh.changed);
-  gulp.watch('bower_components/**').on('change', refresh.changed);
-
-});
-
-gulp.task('default', ['serve', 'livereload', 'watch']);
+require('./gulp');
